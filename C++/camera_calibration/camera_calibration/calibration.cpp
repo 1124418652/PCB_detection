d3a08671dbab6f8d84defc0daa1bc1feb1ae55e1
hejun <1124418652@qq.com>
2019-03-29 16:38:35 +0800

    add function readPatternImg

--- C++/camera_calibration/camera_calibration/calibration.cpp
+++ C++/camera_calibration/camera_calibration/calibration.cpp
@@ -8,6 +8,40 @@ bool CCalibration::saveParams(std::string _calibResultPath)
 	assert(cameraMatrix.rows == 3 && cameraMatrix.cols == 3);
 	cameraMatrix.convertTo(cameraMatrix, CV_32FC1);
 	cameraDistortCoeff.convertTo(cameraDistortCoeff, CV_32FC1);
+
 	std::ofstream fout;
 	fout.open(_calibResultPath + "calibResult.txt", std::ios::out);
+	for (int i = 0; i < 3; ++i)
+	{
+		float *pRow = cameraMatrix.ptr<float>(i);
+		for (int j = 0; j < 3; ++j)
+		{
+			fout << *(pRow++) << '\t';
+		}
+		fout << std::endl;
+	}
+
+#ifdef CV
+	fout << "Distortion coefficients:" << std::endl;
+	fout << cameraDistortCoeff.at<float>(0, 0) << '\t';
+	fout << cameraDistortCoeff.at<float>(0, 1) << '\t';
+	fout << cameraDistortCoeff.at<float>(0, 2) << '\t';
+	fout << cameraDistortCoeff.at<float>(0, 3) << '\t';
+	fout << cameraDistortCoeff.at<float>(0, 4) << '\t';
+#elif define FISHEYE
+	fout << "Distortion coefficients with fisheye camera:" << endl;
+	fout << cameraDistortCoeff.at<float>(0, 0) << '\t';
+	fout << cameraDistortCoeff.at<float>(0, 1) << '\t';
+	fout << cameraDistortCoeff.at<float>(0, 2) << '\t';
+	fout << cameraDistortCoeff.at<float>(0, 3) << '\t';
+#endif
+	fout.close();
+	return true;
+}
+
+bool CCalibration::readPatternImg()
+{
+	cv::Mat img;
+	std::vector<std::string> files;
+	
 }
