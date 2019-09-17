10f3a6b925c7da7f4763b3f837c2180cc301ab6b
hejun <1124418652@qq.com>
2019-09-17 23:10:26 +0800

    add function set_distance_thresh

--- python_algrithom/software/ransac/ransac.py
+++ python_algrithom/software/ransac/ransac.py
@@ -12,7 +12,14 @@ from abc import ABC, abstractmethod
 
 class Ransac(ABC):
 
-	def __init__(self, samples=None):
+	def __init__(self, samples=None, distance_thresh=.1):
+		"""
+		constructor of Ransac
+		Args:
+			samples: numpy.ndarray, the total samples
+			distance_thresh: float, the threshold of distance between inliers 
+							 to the model computed
+		"""
 
 		self.samples = samples
 		self.random_seed = 0
@@ -21,6 +28,7 @@ class Ransac(ABC):
 		if samples is not None:
 			self.global_samples_num = len(samples)
 		self.inlier_thresh = self.global_samples_num // 4
+		self.distance_thresh = distance_thresh
 
 
 	def set_random_seed(self, random_seed):
@@ -32,6 +40,11 @@ class Ransac(ABC):
 	def set_max_iterations(self, max_iterations=0):
 
 		self.max_iterations = max_iterations
+
+
+	def set_distance_thresh(self, distance_thresh):
+
+		self.distance_thresh = distance_thresh
 
 
 	@abstractmethod
